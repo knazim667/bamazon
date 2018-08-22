3df9f88cb669a458bcdd8c9c8a5a87a658c2a24f
Nazam <nazamzahoor@yahoo.com>
2018-08-22 11:52:58 -0700

    done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -33,10 +33,55 @@ function Purchase(){
 
         name : "item",
         type : "input",
-        message : "What would you like to buy? Please provide item id.."
+        message : "What would you like to buy? Please provide item id..",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+            return true;
+            }
+            return false;
+        }
     },
-        { name : "amount",
+    { 
+        name : "chooseQuantity",
         type : "input",
-        message: "How many do you want?"
-    }]).then(function
-}
+        message: "How many do you want?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+            return true;
+            }
+            return false;
+        }
+    }])
+    .then(function(answer){
+        var query = "SELECT id, product_name, stock_quantity FROM products WHERE ?"
+        connection.query(query, [{id : answer.item}],
+        function(err, res) {
+            if (answer.amount > res[0].stock_quantity){
+                console.log("Sorry, The requested product inventory is not Enough.");
+                console.log( " Here are some Left :");
+                console.log(" || item ID: " + res[0].id + " || Product: " + res[0].product_name + " || Current Stock: " + res[0].stock_quantity);
+                readproducts();
+            }else {
+                console.log("Thanks For purchasing!");
+                var query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE ? AND stock_quantity > 0";
+                connection.query(query, [answer.chooseQuantity, {id: answer.item}],
+                function(err, res) {
+                    var query = "SELECT id, product_name, price FROM products WHERE ?";
+                    connection.query(query, [{id : answer.item}],
+                    function(err, res) {
+                        if (err) throw err;
+                        console.log(" || Item Id: " + res[0].id +
+                    " || Product: " + res[0].product_name +
+                    " || Price: $" + res[0].price +
+                    " || Quantity: " + res[0].answer.chooseQuantity + 
+                    " || Total Price: $" + res[0].price * parseFloat(answer.chooseQuantity));
+                    connection.end()}
+                    )
+                }
+                )
+            };
+        }
+        )
+    
+    });
+};
